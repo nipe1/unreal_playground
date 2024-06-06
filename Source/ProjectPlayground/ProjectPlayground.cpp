9f640a1e2c729c0083c9a473a47fd5645a25dfa0
nipe1 <pelkonenniilo@gmail.com>
2024-06-06 17:18:27 +0300

    Initial commit, water physics test + basic landscape

---
+++ Source/ProjectPlayground/ProjectPlayground.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectPlayground.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectPlayground, "ProjectPlayground" );
